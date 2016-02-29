d5b0fbf11efac6b39c9ffb15b3a29dadad3c7584
Daniel Robinson <dan.rob.xor@gmail.com>
2016-02-29 17:09:44 +0000

    final compiler/conf fixes

--- src/server/server.go
+++ src/server/server.go
@@ -8,9 +8,9 @@ import (
 
 func main() {
 	conf := conf.GetAppConf()
-	http.Handle(`/`, conf.RootMux)
+	http.Handle(`/`, conf.Wall)
 	if strings.HasSuffix(conf.FullUrlBase, `https://`) {
-		conf.Log.Info(`Secure Server Listening on Port ` + conf.PortString())
+		conf.Log.Info(`Secure Server Listening on Port ` + conf.PortString)
 		http.ListenAndServeTLS(conf.PortString, conf.CertFile, conf.KeyFile, nil)
 	} else {
 		conf.Log.Critical(`Non Secure Server Listening on Port ` + conf.PortString)
