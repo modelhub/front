dd98a63f57454aa6a0614e9e986be965a0b8d294
Daniel Robinson <dan.rob.xor@gmail.com>
2016-03-28 14:33:06 +0100

    implement breadcrumbs :)

--- src/client/component/breadcrumbs/breadcrumbs.js
+++ src/client/component/breadcrumbs/breadcrumbs.js
@@ -1,13 +1,16 @@
 define('breadcrumbs/breadcrumbs', [
     'styler',
     'text!breadcrumbs/breadcrumbs.css',
-    'text!breadcrumbs/breadcrumbs.html'
+    'text!breadcrumbs/breadcrumbs.html',
+    'text!breadcrumbs/breadcrumbs.txt.json'
 ], function(
     styler,
     style,
-    tpl
+    tpl,
+    txt
 ){
     styler(style);
+    txt = JSON.parse(txt);
 
     return function(ngModule){
         ngModule
@@ -19,8 +22,51 @@ define('breadcrumbs/breadcrumbs', [
                         entityType: '@',
                         entityId: '@'
                     },
-                    controller: ['$scope', function($scope){
+                    controller: ['$element', '$location', '$scope', '$window', 'api', 'i18n', function($element, $location, $scope, $window, api, i18n){
+                        i18n($scope, txt);
 
+                        $scope.rootParent = '00000000000000000000000000000000';
+
+                        var rootEl = $element[0].getElementsByClassName('root')[0],
+                            errorHandler = function(errorId){
+                                $scope.errorId = errorId;
+                            },
+                            nodeGetters = function(leafNodeId) {
+                                var leafNode;
+                                api.v1.treeNode.get([leafNodeId]).then(function (entities) {
+                                    leafNode = entities[0];
+                                    api.v1.project.get([entities[0].project]).then(function (projects) {
+                                        $scope.project = projects[0];
+                                    }, errorHandler);
+
+                                    api.v1.treeNode.getParents(leafNodeId).then(function (parents) {
+                                        $scope.parents = parents;
+                                        $scope.parents.push(leafNode);
+                                        $window.setTimeout(function(){
+                                            rootEl.scrollLeft = rootEl.scrollWidth;
+                                        }, 0);
+                                    }, errorHandler);
+                                }, errorHandler);
+                            };
+
+                        switch ($scope.entityType) {
+                            case 'folder':
+                            case 'document':
+                                nodeGetters($scope.entityId);
+                                break;
+                            case 'documentVersion':
+                                api.v1.documentVersion.get([$scope.entityId]).then(function(entities){
+                                    $scope.versionEntity = entities[0];
+                                    nodeGetters(entities[0].document);
+                                }, errorHandler);
+                                break;
+                            default:
+                                throw 'unknown entity type';
+                        }
+
+                        $scope.treeNodeClick = function(node){
+                            $location.path('/'+node.nodeType+'/'+node.id);
+                        };
                     }]
                 };
             });
